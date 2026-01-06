b3877a4d89ead7d4d658ef6f0e66a60e877295fc
Forrest Gentry <gentryf@submit-b.ib.coehpc>
2026-01-05 20:22:39 -0800

    adjusted comments

--- prog1.c
+++ prog1.c
@@ -5,10 +5,10 @@
 
 
 int main() {
-	srand(time(NULL)); // Seed the random number generator
+	srand(time(NULL)); // Seed the prng
 	int myrand = rand() % 11; // Generate a random number between 0 and 10 inclusive
 
-	// Print different messages based on the value of myrand
+	// Print different messages based on the random value
 	if (myrand >= 0 && myrand <= 4) {
 		printf("Eat more beef, kick less cats\n");
 	} else if (myrand >= 5 && myrand <= 9) {
@@ -17,7 +17,7 @@ int main() {
 		printf("Larn is the best roguelike\n");
 	}
 
-	// Print the generated random number
+	// Print the random number
 	printf("The random number was: %d\n", myrand);
 
 	return 0;
