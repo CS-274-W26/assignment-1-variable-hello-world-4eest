ba51f4e86225f1aa936ba1520b7165721d2a6a26
Forrest Gentry <gentryf@submit-b.ib.coehpc>
2026-01-05 20:21:20 -0800

    Finished Assignment Program

--- prog1.c
+++ prog1.c
@@ -1,11 +1,24 @@
 #include <stdio.h> // Necessary for printf()
 
-// TODO Other includes
+#include <stdlib.h> // Necessary for rand() and srand()
+#include <time.h> // Necessary for time()
 
 
 int main() {
-	// The main function is the entrypoint of the program. When the program
-	// is executed, it will automatically start here.
-	
-	// TODO Complete the program
+	srand(time(NULL)); // Seed the random number generator
+	int myrand = rand() % 11; // Generate a random number between 0 and 10 inclusive
+
+	// Print different messages based on the value of myrand
+	if (myrand >= 0 && myrand <= 4) {
+		printf("Eat more beef, kick less cats\n");
+	} else if (myrand >= 5 && myrand <= 9) {
+		printf("FRODO LIVES\n");
+	} else if (myrand == 10) {
+		printf("Larn is the best roguelike\n");
+	}
+
+	// Print the generated random number
+	printf("The random number was: %d\n", myrand);
+
+	return 0;
 }
